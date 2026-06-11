32af3f07b8af4b3bc6e983db2524e40d82af7822
nguyenphong233 <nguyenphong230306@gmail.com>
2026-06-11 08:30:36 +0700

    update host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    allowedHosts: true
+    allowedHosts: true,
   },
   plugins: [
     react(),
